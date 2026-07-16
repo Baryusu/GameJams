c454a502ad93ad428ad0cc193fc2cce91c55a101
DPTDM <22105026@usc.edu.ph>
2026-07-16 11:48:37 +0800

    Journal Fix Added

--- ui/journal/journal_ui.gd
+++ ui/journal/journal_ui.gd
@@ -1,15 +1,14 @@
 extends CanvasLayer
 
-@onready var left_page = $LeftPage
-@onready var right_page = $RightPage
+@onready var left_page = $Control/HBoxContainer/Control/LeftPageText
+@onready var right_page = $Control/HBoxContainer/Control2/RightPageText
+
 # This keeps track of what page the left side of the book is currently showing
 var current_left_page_index: int = 0
 
 func _ready() -> void:
-	# Hide the journal when the game starts
-	hide()
+	hide()  # Hide the journal when the game starts
 
-# Call this function from your Player script when they press "J" or click the journal!
 func open_journal() -> void:
 	current_left_page_index = 0
 	update_book_display()
@@ -19,40 +18,36 @@ func close_journal() -> void:
 	hide()
 
 func _input(event: InputEvent) -> void:
-	# If the journal isn't visible, ignore keyboard inputs for flipping pages
 	if not visible:
 		return
-		
+
 	var total_notes = GlobalData.journal.size()
-	
-	# Listen for D or Right Arrow
+
 	if event.is_action_pressed("ui_right"):
-		# Only flip forward if there are more notes to read
 		if current_left_page_index + 2 < total_notes:
 			turn_page_forward()
-			
-	# Listen for A or Left Arrow
+
 	if event.is_action_pressed("ui_left"):
-		# Only flip backward if we aren't already on the very first page
 		if current_left_page_index > 0:
 			turn_page_back()
 
 func update_book_display() -> void:
 	var notes = GlobalData.journal
 	var total_notes = notes.size()
-	
-	# --- RENDER THE LEFT PAGE ---
+
+	# Left page
 	if current_left_page_index < total_notes:
-		# Using your BBCode formatting for the title!
+		left_page.bbcode_enabled = true
 		left_page.text = "[center][b]" + notes[current_left_page_index]["title"] + "[/b][/center]\n\n" + notes[current_left_page_index]["text"]
 	else:
-		left_page.text = "" # Blank page if no note exists
-		
-	# --- RENDER THE RIGHT PAGE ---
+		left_page.text = ""
+
+	# Right page
 	if current_left_page_index + 1 < total_notes:
+		right_page.bbcode_enabled = true
 		right_page.text = "[center][b]" + notes[current_left_page_index + 1]["title"] + "[/b][/center]\n\n" + notes[current_left_page_index + 1]["text"]
 	else:
-		right_page.text = "" # Blank page if no note exists
+		right_page.text = ""
 
 func turn_page_forward() -> void:
 	current_left_page_index += 2
